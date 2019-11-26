06efb9b921e9add5c855eec26d4844f5f2e8a56a
Raphaël QUEIROZ <queiro_r@epita.fr>
2019-11-26 02:01:31 +0100

    up

--- front/src/components/Animations.vue
+++ front/src/components/Animations.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <Anim name="teamfilm" />
+    <Anim
+      :width="800"
+      :height="800"
+      name="teamfilm"
+    />
     <Anim
       :width="800"
       :height="800"
