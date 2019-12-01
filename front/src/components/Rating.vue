89671f080d0752de76826371ab4ca79e9164101f
Raphaël QUEIROZ <queiro_r@epita.fr>
2019-12-01 02:54:13 +0100

    show rating

--- front/src/components/Rating.vue
+++ front/src/components/Rating.vue
@@ -1,13 +1,18 @@
 <template>
-  <v-rating
+  <v-row
     v-if="rating > -1"
-    v-model="rating"
-    color="yellow darken-3"
-    background-color="grey darken-1"
-    half-increments
-    readonly
-    dense
-  ></v-rating>
+    align="center"
+  >
+    <v-rating
+      v-model="rating"
+      color="yellow darken-3"
+      background-color="grey darken-1"
+      half-increments
+      readonly
+      dense
+    ></v-rating>
+    <v-chip small>{{ Math.round(rating * 100) / 100 }} / 5</v-chip>
+  </v-row>
   <span v-else>
     No votes
   </span>
