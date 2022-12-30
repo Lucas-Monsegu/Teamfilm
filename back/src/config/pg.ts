ff01ed4780c76f597b03f726b9bf1da2343002cd
Lucas Monsegu <lucas.monsegu@budget-insight.com>
2022-12-30 15:05:48 +0100

    feat: make pgport a number

--- back/src/config/pg.ts
+++ back/src/config/pg.ts
@@ -1,8 +1,15 @@
 
+console.log({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT)
+})
 export default {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
+  port: Number(process.env.PGPORT)
 }
