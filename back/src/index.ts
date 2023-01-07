71153d78834a603d5b66387c7240e49cca0978c2
Lucas Monsegu <lucas.monsegu@budget-insight.com>
2023-01-07 23:10:39 +0100

    fix: auth

--- back/src/index.ts
+++ back/src/index.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 app.use(cors({ origin: ['http://localhost:8080', 'https://teamfilm.vercel.app'], credentials: true },));
-app.use(session({ secret: process.env.SECRETSESSION, resave: false, saveUninitialized: true, cookie: { maxAge: 6000 * 60 * 24 * 6 * 7, sameSite: 'none', httpOnly: true, secure: true } }))
+app.use(session({ secret: process.env.SECRETSESSION, resave: false, saveUninitialized: false, cookie: { maxAge: 6000 * 60 * 24 * 6 * 7, sameSite: 'none', secure: true } }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use("/api", misc)
